7cdcf52368a30a7cd07a900afcb9ae8c39f11005
Anurag Gupta <anurag.iiits@gmail.com>
2017-12-16 08:27:56 +0530

    Insert data using faker

--- imports/collections/employee.js
+++ imports/collections/employee.js
@@ -1,4 +1,4 @@
 import {Mongo} from 'meteor/mongo';
 
-export const Employee = new Mongo.Collection('employees'); 
+export const Employees = new Mongo.Collection('employees'); 
 //not export default because multiple variables are to be exported
--- server/main.js
+++ server/main.js
@@ -1,6 +1,27 @@
 //only executed on the server
 import { Meteor } from 'meteor/meteor';
+import _ from 'lodash';
+import {Employees} from '../imports/collections/employee'
+import {image, helpers} from 'faker';
 
 Meteor.startup(() => {
   // code to run on server at startup hence great place to generate data
+  //check if data already exists
+  //count the number of records in database
+  const numberRecords = Employees.find({}).count();
+  //specify the filter options in find, here no filter required
+  console.log(numberRecords);
+  if(!numberRecords){
+    //generate the records using faker
+    
+    _.times(5000, ()=>{
+      const {name, email, phone} = helpers.createCard();
+
+      Employees.insert({
+        name, email, phone,
+        avatar: image.avatar()
+      }); //name, email,... equivalent to name:name, email:email,...
+    });
+    
+  }
 });
