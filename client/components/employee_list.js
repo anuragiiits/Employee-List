3e3475fe27b2b3cae46c08d0b0d759d13b615f46
Anurag Gupta <anurag.iiits@gmail.com>
2017-12-20 11:32:22 +0530

    Application finished

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Employees } from '../../imports/collections/employee';
 import { createContainer } from 'meteor/react-meteor-data'
 import EmployeeDetail from './employee_detail';
 
-const EmployeeList = (props) => {
-    //props.employees => an array of employee objects
-    console.log(props.employees);
-    return (
-        <div>
-            <div className="employee-list">
-                 {  props.employees.map((employee) => 
-                    <EmployeeDetail key={employee._id} employee={employee}/>
-                    )}
+const PER_PAGE = 20;
+
+class EmployeeList extends Component{
+    componentWillMount(){
+        this.page=1;
+    }
+    onClickPage(){
+        Meteor.subscribe('employees', PER_PAGE*(this.page+1));
+        this.page+=1;
+    }
+    render(){
+    //this.props.employees => an array of employee objects
+    //console.log(this.props.employees);
+        return (
+            <div>
+                <h2 className="text text-primary text-center"> My Employees </h2>
+                <div className="employee-list well">
+                    
+                    {  this.props.employees.map((employee) => 
+                        <EmployeeDetail key={employee._id} employee={employee}/>
+                        )}
+                </div>
+                <button onClick= {this.onClickPage.bind(this)}
+                    className="btn btn-primary">
+                    Load more...
+                </button>
             </div>
-        </div>
-    )
+        );
+    };
 };
 
 export default createContainer(()=>{
     //set up the subscription
-    Meteor.subscribe('employees');
+    Meteor.subscribe('employees', PER_PAGE); //sends the argument PER_PAGE
 
     //return an object. Whatever is returned will be sent to EmployeeList
     // as props
--- server/main.js
+++ server/main.js
@@ -26,7 +26,7 @@ Meteor.startup(() => {
     
   }
 
-  Meteor.publish('employees', function(){
-    return Employees.find({},{limit:20});
+  Meteor.publish('employees', function(per_page){   //receives the argument per_page
+    return Employees.find({},{limit:per_page});
   });
 });
